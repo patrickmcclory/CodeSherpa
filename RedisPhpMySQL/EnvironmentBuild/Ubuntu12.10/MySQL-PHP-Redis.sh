5c8129d0ab5c7f9dcfa7a0061a761f31ef79c0d4
Patrick McClory <patrick@rightscale.com>
2013-03-25 19:20:19 -0400

    committing initial bash script for install
    Install script for demo environment… assumes a Ubuntu 12.10 install w/
    mysql 5.5, php5 and redis 2.6.11

---
+++ RedisPhpMySQL/EnvironmentBuild/Ubuntu12.10/MySQL-PHP-Redis.sh
@@ -0,0 +1,18 @@
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.5 mysql-server/root_password password P@ssword1 | sudo debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password P@ssword1 | sudo debconf-set-selections
+sudo apt-get install mysql-server -y
+sudo apt-get install make gcc -y
+wget http://redis.googlecode.com/files/redis-2.6.11.tar.gz
+sudo tar xzf redis-2.6.11.tar.gz -C /usr/lib
+cd /usr/lib/redis-2.6.11
+sudo make
+sudo ln -s /usr/lib/redis-2.6.11/src/redis-server /usr/local/bin/redis-server
+sudo ln -s /usr/lib/redis-2.6.11/src/redis-cli /usr/local/bin/redis-cli
+cd /usr/lib/redis-2.6.11/utils
+echo '' | sudo ./install_server.sh
+apt-get install apache2
+apt-get install php5 libapache2-mod-php5
+/etc/init.d/apache2 restart
+apt-get install php5-mysql php5-curl php5-xcache
+/etc/init.d/apache2 restart
