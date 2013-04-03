8b254a1d747c9ea5a50987c0657873eda42397e7
Patrick McClory <patrick@rightscale.com>
2013-04-03 10:41:41 -0400

    quick update to script from testing

--- RabbitMQPhpMySQL/EnvironmentBuild/Ubuntu12.10/install.sh
+++ RabbitMQPhpMySQL/EnvironmentBuild/Ubuntu12.10/install.sh
@@ -11,23 +11,28 @@ apt-get install php5-mysql php5-curl php5-xcache php5-cli -y
 cd /~
 echo -e "<?php\nphpinfo();\n?>" >> info.php
 mv info.php /var/www/info.php
-apt-get install rabbimq-server
-git clone git://github.com/videlalvaro/php-ampqlib.git
+apt-get install rabbitmq-server -y
+git clone git://github.com/vidlalvaro/php-ampqlib.git
 cd /~/php-amqplib
 curl --silent https://getcomposer.org/installer | php
 php composer.phar install
 cd /~ 
-apt-get install gcc -y
-mkdir -p package
-chmod 1755 package
-cd package
-wget http://cr.yp.to/daemontools/daemontools-0.76.tar.gz
-gunzup daemontools-0.76.tar.gz
-tar -xpf daemontools-0.76.tar
-rm -f daemontools-0.76.tar
-cd admin/daemontools-0.76
-sed -ie 's/^\(gcc.*\)/\1 -include \/usr\/include\/errno.h' src/conf-cc
-package/install
+apt-get install daemontools daemontools-run -y
+cd /~
+git clone git://github.com/patrickmcclory/CodeSherpa.git
+cd /~
+git config core.sparsecheckout true
+echo /RabbitMQPhpMySQL/ > .git/info/sparse-checkout
+git read-tree -m -u HEAD
+mysql -pP@ssword1 -e "CREATE DATABASE demoDataStore;"
+mysql -pP@ssword1 << QUERY_INPUT
+use demoDataStore;
+CREATE TABLE dataStore(
+	uniqueID char(23), 
+	randomChar1 char(5),
+	randomChar2 char(5)
+)
+QUERY_INPUT
 echo
 echo 'You can SSH into this instance on the address below:'
 ifconfig | grep 'inet addr' | grep -v '127.0.'
